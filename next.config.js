2833c440b0c062c25b84317cfd044c9e4b6e1e73
adetyaz <tayo.lasisi@yahoo.com>
2022-12-09 11:57:18 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	webpack: function (config, options) {
+		config.experiments = {
+			asyncWebAssembly: true,
+		}
+		return config
+	},
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MeshProvider } from '@martifylabs/mesh-react'
+MeshProvider
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<MeshProvider>
+			<Component {...pageProps} />
+		</MeshProvider>
+	)
 }
